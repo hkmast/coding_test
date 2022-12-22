0ac130ff466c5e8ad427856121eab84c3fa85106
hkmast <kms970910@naver.com>
2022-12-22 10:00:45 +0900

    12_22_2

--- 정렬1.py
+++ 정렬1.py
@@ -24,14 +24,14 @@ array	commands	return
 
 """
 
-
 def solution(array, commands):
 
     answer = []
 
     for command in commands:
-        tmp = array[command[0]-1:command[1]-1]
+        tmp = array[command[0]-1:command[1]]
         tmp.sort()
-        answer.append(tmp[command[2]])
+        answer.append(tmp[command[2]-1])
 
     return answer
+
