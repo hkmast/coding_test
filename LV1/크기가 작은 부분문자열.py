da12b65a977df450ab029cec11687b8197ddd6b5
hkmast <kms970910@naver.com>
2022-12-23 16:34:39 +0900

    12_23_5

---
+++ LV1/크기가 작은 부분문자열.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/147355
+
+def solution(t, p):
+    answer = 0
+
+    # print('p', p)
+    length = len(p)
+    p = int(p)
+    for i in range(0, len(t) - length + 1):
+        t_sliced = int(t[i:i + length])
+        # print('sliced', t_sliced)
+        if t_sliced <= p:
+            answer += 1
+
+
+    # print('ans', answer)
+    return answer
