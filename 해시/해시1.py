de0a089933c362ad1c6dfa17bb62a639d7480dc2
hkmast <kms970910@naver.com>
2022-12-26 17:08:18 +0900

    12_26_2

--- 해시/해시1.py
+++ 해시/해시1.py
@@ -1,24 +1,46 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/1845
+# https://school.programmers.co.kr/learn/courses/30/lessons/42576
 
-# 제일 깔끔한 버전
+# 풀이1
+# 효율성 떨어짐
+# 예상 시간 복잡도 log n^2~3
 '''
-def solution(nums):
-    return min(len(nums)/2, len(set(nums)))
-'''
+def solution(participants, completions):
+    answer = ''
 
-def solution(nums):
-    answer = 0
-    N = len(nums)
-    dic = dict()
+    for completion in completions:
+        del participants[participants.index(completion)]
 
-    for num in nums:
-        if not dic.get(num):
-            dic[num] = 1
-        else:
-            dic[num] += 1
-
-    cat = len(dic)
-
-    answer = min(N/2, cat)
+    answer = participants[0]
 
     return answer
+'''
+def solution(participants, completions):
+
+    participants_dict = dict()
+    for participant in participants:
+        if not participants_dict.get(participant):
+            participants_dict[participant] = 1
+        else:
+            participants_dict[participant] += 1
+
+    completions_dict = dict()
+    for completion in completions:
+        if not completions_dict.get(completion):
+            completions_dict[completion] = 1
+        else:
+            completions_dict[completion] += 1
+
+    print(participants_dict)
+    print(completions_dict)
+
+    for completion in completions_dict:
+
+        participants_dict[completion] = participants_dict[completion] - completions_dict[completion]
+        if participants_dict[completion] == 1:
+            answer = completion
+            return answer
+
+    for participant in participants_dict:
+        if participants_dict[participant] == 1:
+            answer = participant
+            return answer
