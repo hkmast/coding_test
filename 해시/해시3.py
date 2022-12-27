0c87e91d9039617814b98724133e03e988c03962
hkmast <kms970910@naver.com>
2022-12-27 10:14:01 +0900

    12_27_1

--- 해시/해시3.py
+++ 해시/해시3.py
@@ -1,35 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42577
-
 def solution(phone_book):
-    answer = True
+    phone_hash = {}
 
-    phone_book_dic = {}
-
+    len_min = len(phone_book[0])
     for phone in phone_book:
-        if not phone_book_dic.get(len(phone)):
-            phone_book_dic[len(phone)] = [phone]
-        else:
-            phone_book_dic[len(phone)].append(phone)
+        phone_hash[phone] = 1
+        if len(phone) < len_min: len_min = len(phone)
 
-    # print(phone_book_dic)
+    for phone in phone_hash:
+        for i in range(len_min, len(phone)):
+            if phone[:i] in phone_hash:
+                return False
 
-    for i in range(1, 20):
-        if not phone_book_dic.get(i):
-            continue
-        else:
-            for phone in phone_book_dic[i]:
-                for j in range(i+1, 20 + 1):
-                    if not phone_book_dic.get(j):
-                        continue
-                    else:
-                        for phone2 in phone_book_dic[j]:
-                            if phone == phone2[:len(phone)]:
-                                answer = False
-                                return answer
-
-
-    return answer
-
-print(solution(["119", "97674223", "1195524421"]))
-
-
+    return True
