4bfb3300fddf1fdbf929ecfd469c6cbdd93e577a
hkmast <kms970910@naver.com>
2023-01-10 15:10:27 +0900

    01_10_1

---
+++ 스택_큐/스택_큐4.py
@@ -0,0 +1,31 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42587
+def solution(priorities, location):
+
+    answer = 0
+
+    priorities = [(priorities[x], x==location) for x in range(len(priorities))]
+
+    #print(priorities)
+
+    cnt = 0
+
+    while priorities:
+        print(priorities, cnt)
+        tmp = priorities.pop(0)
+        if priorities:
+            if tmp[0] < max(priorities)[0]:
+                priorities.append(tmp)
+            else:
+                cnt += 1
+                if tmp[1]: break
+
+        else:
+            cnt += 1
+            if tmp[1]: break
+
+    answer = cnt
+
+    return answer
+
+
+print(solution([1, 1, 9, 1, 1, 1],0))
