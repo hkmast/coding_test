e67539bf25fb861e4fe9348da8681025867eb7c4
hkmast <kms970910@naver.com>
2023-01-30 15:04:35 +0900

    01_30_1

--- 스택_큐/스택_큐6.py
+++ 스택_큐/스택_큐6.py
@@ -1,3 +1,5 @@
+# 속도가 느림 -> 효율성 X
+"""
 def solution(prices):
 
     answer = []
@@ -5,6 +7,7 @@ def solution(prices):
     for idx, price in enumerate(prices):
 
         cnt = 0
+        
         for comp_price in prices[idx+1:]:
             cnt += 1
             if price > comp_price:
@@ -14,3 +17,24 @@ def solution(prices):
 
 
     return answer
+"""
+
+
+def solution(prices):
+    LENGTH = len(prices)
+
+    answer = [0] * LENGTH
+
+    print(answer)
+
+    for i in range(0, LENGTH - 1):
+        for j in range(i + 1, LENGTH):
+            if prices[i] > prices[j]:
+                answer[i] = j - i
+                break
+            answer[i] = j - i
+
+    return answer
+
+
+print(solution([1, 2, 3, 2, 3]))
